73252df1d7d49a8cca814c39933cc66fb9d80ce4
Dylan Thomson <djthomson88@gmail.com>
2017-07-17 16:06:18 -0400

    sumArray() testing
    -Console.log test cases for sumArray()

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ function sumAll(arr) {
 	return (((max - min + 1) * (min + max)) / (2));
 }
 
+console.log("sumAll([1, 4]) = " + sumAll([1, 4]));
+console.log("sumAll([4, 1]) = " + sumAll([4, 1]));
+console.log("sumAll([5, 10]) = " + sumAll([5, 10]));
+console.log("sumAll([10, 5]) = " + sumAll([10, 5]));
