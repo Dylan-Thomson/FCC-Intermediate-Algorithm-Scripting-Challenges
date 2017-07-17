e20a99d065c7da1385b9d9d6e4b9930007d59291
Dylan Thomson <djthomson88@gmail.com>
2017-07-17 13:27:27 -0400

    Update sumAll()

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 /*********************************************************************	
 	SUM ALL
 **********************************************************************/
+// Use spread syntax to pass arrays to Math.min() and Math.max() 
 // function sumAll(arr) {
-//   var max = arr.reduce(function(a, b) {
-//     return Math.max(a, b);
-//   });
-//   var min = arr.reduce(function(a, b) {
-//     return Math.min(a, b);
-//   });
-//   var result = (max - min + 1) * (max + min)/2;
-//   return result;
+//     var sum = 0;
+//     for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+//         sum += i;
+//     }
+//   return sum;
 // }
 
-// Use spread syntax to pass arrays to Math.min() and Math.max()
+// Use spread syntax to get min/max, and arithmetic progression to solve sum
 function sumAll(arr) {
-    var sum = 0;
-    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
-        sum += i;
-    }
-  return sum;
+	const min = Math.min(...arr);
+	const max = Math.max(...arr);
+	return (((max - min + 1) * (min + max)) / (2));
 }
 
 sumAll([1, 4]);
